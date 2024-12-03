a2a9a4d801cd2116cc4702a517c6b2dca1f1eecf
Jack Myers <jackhyers97@gmail.com>
2024-12-02 22:34:04 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- llvm/scripts/test.sh
+++ llvm/scripts/test.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-export CPATH="$GCC_INSTALL_DIR/include/c++:$GCC_INSTALL_DIR/include/c++/$CONDA_TOOLCHAIN_HOST:$PREFIX/include:$CONDA_CUDA_ROOT/include:$CONDA_BUILD_SYSROOT/usr/include"
 export LD_LIBRARY_PATH="$LLVM_SYCL_BUILD_DIR/lib:$PREFIX/lib:$LD_LIBRARY_PATH"
-python llvm/buildbot/check.py -o "$LLVM_SYCL_BUILD_DIR"
+cmake --build "$LLVM_SYCL_BUILD_DIR" -t check-all
