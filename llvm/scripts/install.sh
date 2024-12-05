ee9a7a3f8f9ac4990f693c2f5ad801ee0e71f1f6
Jack Myers <jack.myers@intel.com>
2024-12-05 20:39:02 +0000

    WIP
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- llvm/scripts/install.sh
+++ llvm/scripts/install.sh
@@ -7,14 +7,18 @@ python llvm/buildbot/compile.py -o "$LLVM_SYCL_BUILD_DIR"
 cmake --build "$LLVM_SYCL_BUILD_DIR"
 cmake --build "$LLVM_SYCL_BUILD_DIR" --target install
 
+rm -rf "$INSTALL_PREFIX"
 mkdir -p "$INSTALL_PREFIX"
 cp -r "$SUBPROJECT_ROOT"/files/* "$INSTALL_PREFIX"
-find "$LLVM_SYCL_BUILD_DIR/install" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX" ";"
-find "$LLVM_SYCL_BUILD_DIR/bin" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX/bin" ";"
-find "$LLVM_SYCL_BUILD_DIR/lib" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX/lib" ";"
-find "$LLVM_SYCL_BUILD_DIR/include" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX/include" ";"
-find "$LLVM_SYCL_BUILD_DIR/share" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX/share" ";"
-find "$LLVM_SYCL_BUILD_DIR/libexec" -maxdepth 1 -exec cp -r {} "$INSTALL_PREFIX/libexec" ";"
+cp -r "$LLVM_SYCL_BUILD_DIR"/lib "$INSTALL_PREFIX"
+cp -r "$LLVM_SYCL_BUILD_DIR"/include "$INSTALL_PREFIX"
+cp -r "$LLVM_SYCL_BUILD_DIR"/bin "$INSTALL_PREFIX"
+cp -r "$LLVM_SYCL_BUILD_DIR"/share "$INSTALL_PREFIX"
+cp -r "$LLVM_SYCL_BUILD_DIR"/libexec "$INSTALL_PREFIX"
+
+# merge "$LLVM_SYCL_BUILD_DIR"/install with "$INSTALL_PREFIX"
+# including files prefixed with .
+cp -r "$LLVM_SYCL_BUILD_DIR"/install/. "$INSTALL_PREFIX"
 
 if [ -z "$NO_PATCHELF" ]; then
   NO_PATCHELF=0
