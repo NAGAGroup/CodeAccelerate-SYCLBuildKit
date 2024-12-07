d035985f8d89801a3fe6905a27307d7780a95bb7
Jack Myers <jackhyers97@gmail.com>
2024-12-07 14:28:13 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- llvm/scripts/install.sh
+++ llvm/scripts/install.sh
@@ -9,15 +9,6 @@ cmake --build "$LLVM_SYCL_BUILD_DIR" --target install
 
 rm -rf "$INSTALL_PREFIX"
 mkdir -p "$INSTALL_PREFIX"
-cp -r "$SUBPROJECT_ROOT"/files/* "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/lib "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/include "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/bin "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/share "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/libexec "$INSTALL_PREFIX"
-
-# merge "$LLVM_SYCL_BUILD_DIR"/install with "$INSTALL_PREFIX"
-# including files prefixed with .
 cp -r "$LLVM_SYCL_BUILD_DIR"/install/. "$INSTALL_PREFIX"
 
 if [[ "${NO_PATCHELF:-0}" != 1 ]]; then
