ae1104dadba7f38733eb1cdc09869b9665d68050
Jack Myers <jackhyers97@gmail.com>
2024-12-08 14:50:46 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- llvm/scripts/install.sh
+++ llvm/scripts/install.sh
@@ -11,7 +11,7 @@ if [[ "${NO_PATCHELF:-0}" != 1 ]]; then
   bash "$SUBPROJECT_ROOT/scripts/patch_installed_rpaths.sh"
 fi
 
-if [[ "${CONDA_PROD_BUILD:-0}" != 1 ]]; then
+if [[ "${CONDA_BUILD:-0}" != 1 ]]; then
   for file in "$SUBPROJECT_ROOT/files"/*; do cp -r "$file" "$INSTALL_PREFIX"; done
   for file in "$SUBPROJECT_ROOT/files"/.*; do cp -r "$file" "$INSTALL_PREFIX"; done
 fi
