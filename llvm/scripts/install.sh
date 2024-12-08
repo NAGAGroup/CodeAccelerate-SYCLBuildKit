e513dbad9350a21bf04fe160cd59cf50f66011c0
Jack Myers <jackhyers97@gmail.com>
2024-12-08 14:49:41 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- llvm/scripts/install.sh
+++ llvm/scripts/install.sh
@@ -6,12 +6,12 @@ cd "$SUBPROJECT_ROOT"
 python llvm/buildbot/compile.py -o "$LLVM_SYCL_BUILD_DIR"
 cmake --build "$LLVM_SYCL_BUILD_DIR"
 cmake --build "$LLVM_SYCL_BUILD_DIR" --target install
-cp -r "$SUBPROJECT_ROOT/files"/. "$INSTALL_PREFIX"
-
-rm -rf "$INSTALL_PREFIX"
-mkdir -p "$INSTALL_PREFIX"
-cp -r "$LLVM_SYCL_BUILD_DIR"/install/. "$INSTALL_PREFIX"
 
 if [[ "${NO_PATCHELF:-0}" != 1 ]]; then
   bash "$SUBPROJECT_ROOT/scripts/patch_installed_rpaths.sh"
+fi
+
+if [[ "${CONDA_PROD_BUILD:-0}" != 1 ]]; then
+  for file in "$SUBPROJECT_ROOT/files"/*; do cp -r "$file" "$INSTALL_PREFIX"; done
+  for file in "$SUBPROJECT_ROOT/files"/.*; do cp -r "$file" "$INSTALL_PREFIX"; done
 fi
