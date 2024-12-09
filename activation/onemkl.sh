57cfc798cc3335d6f66660c350a91b83db80578a
Jack Myers <jackhyers97@gmail.com>
2024-12-08 16:28:17 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- activation/onemkl.sh
+++ activation/onemkl.sh
@@ -4,7 +4,7 @@ if [ -z "$DPCPP_ROOT" ]; then
   export DPCPP_ROOT="$HOME/dpcpp"
 fi
 
-if [ -z "$PIXI_DPCPP_ACTIVE" ]; then
+if [ -z "$ONEMKL_BUILD_ENV_ACTIVE" ]; then
   if [ -z "$SUBPROJECT_ROOT" ]; then
     export SUBPROJECT_ROOT="$PIXI_PROJECT_ROOT/onemkl"
   fi
@@ -19,5 +19,5 @@ if [ -z "$PIXI_DPCPP_ACTIVE" ]; then
 
   export OCL_ICD_VENDORS="$PREFIX/etc/OpenCL/vendors"
   export OCL_ICD_FILENAMES=
-  export PIXI_DPCPP_ACTIVE=1
+  export ONEMKL_BUILD_ENV_ACTIVE=1
 fi
