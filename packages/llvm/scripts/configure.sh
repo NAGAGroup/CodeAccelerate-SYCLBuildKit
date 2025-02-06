429478d9be042826f1c5d9a45834c3e5197630b9
Jack <jackhmyers97@gmail.com>
2025-02-05 21:35:03 -0800

    WIP
    Signed-off-by: Jack <jackhmyers97@gmail.com>

--- packages/llvm/scripts/configure.sh
+++ packages/llvm/scripts/configure.sh
@@ -12,48 +12,44 @@ if [ -z "$PROJECT_ROOT" ]; then
   source "$DPCPP_HOME/activation/llvm.sh"
 fi
 
-if [ ! -f "$DPCPP_HOME/repo/build/CMakeCache.txt" ]; then
-  mkdir -p "$DPCPP_BIN_DIR"
-  # find "$DPCPP_BIN_DIR" -name "CMakeCache.txt" -exec rm {} ";"
-  # else
-  # rm -rf "$DPCPP_HOME/repo"
-  # cd "$DPCPP_HOME/../.."
-  # mkdir -p "$DPCPP_HOME/repo"
+if [ -f "$DPCPP_HOME/repo/build/CMakeCache.txt" ]; then
+  find "$DPCPP_BIN_DIR" -name "CMakeCache.txt" -exec rm {} ";"
+fi
 
-  # git submodule update --init --recursive --depth=1
+mkdir -p "$DPCPP_BIN_DIR"
 
-  cd "$DPCPP_HOME"
+cd "$DPCPP_HOME"
 
-  conda_extra_cflags="--sysroot=$CONDA_BUILD_SYSROOT --gcc-install-dir=$GCC_INSTALL_DIR"
-  clang_ldflags="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib -Wl,-rpath,$DPCPP_BIN_DIR/lib -Wl,-rpath-link,$DPCPP_BIN_DIR/lib -L$DPCPP_BIN_DIR/lib -L$CONDA_CUDA_ROOT/lib -L$CONDA_CUDA_ROOT/lib/stubs"
+conda_extra_cflags="--sysroot=$CONDA_BUILD_SYSROOT --gcc-install-dir=$GCC_INSTALL_DIR"
+clang_ldflags="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib -Wl,-rpath,$DPCPP_BIN_DIR/lib -Wl,-rpath-link,$DPCPP_BIN_DIR/lib -L$DPCPP_BIN_DIR/lib -L$CONDA_CUDA_ROOT/lib -L$CONDA_CUDA_ROOT/lib/stubs"
 
-  mkdir -p "$DPCPP_BIN_DIR/bin"
-  echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang++.cfg"
-  echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang-cpp.cfg"
-  echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang.cfg"
+mkdir -p "$DPCPP_BIN_DIR/bin"
+echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang++.cfg"
+echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang-cpp.cfg"
+echo "$conda_extra_cflags" >"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang.cfg"
 
-  echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang++.cfg"
-  echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang-cpp.cfg"
-  echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang.cfg"
+echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang++.cfg"
+echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang-cpp.cfg"
+echo "$clang_ldflags" >>"$DPCPP_BIN_DIR/bin/$CONDA_TOOLCHAIN_HOST-clang.cfg"
 
-  cmake_args=(
-    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-    -DLLVM_DEFAULT_TARGET_TRIPLE="$CONDA_TOOLCHAIN_HOST"
-    -DLLVM_HOST_TRIPLE="$CONDA_TOOLCHAIN_HOST"
-    -DLLVM_INSTALL_UTILS=ON
-    -DLLVM_UTILS_INSTALL_DIR=libexec/llvm
-    -DLLVM_LIBDIR_SUFFIX=""
-    -DCMAKE_TOOLCHAIN_FILE="$PROJECT_TOOLCHAIN_FILE")
+cmake_args=(
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX"
+  -DCMAKE_C_COMPILER_LAUNCHER=ccache
+  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+  -DLLVM_DEFAULT_TARGET_TRIPLE="$CONDA_TOOLCHAIN_HOST"
+  -DLLVM_HOST_TRIPLE="$CONDA_TOOLCHAIN_HOST"
+  -DLLVM_INSTALL_UTILS=ON
+  -DLLVM_UTILS_INSTALL_DIR=libexec/llvm
+  -DLLVM_LIBDIR_SUFFIX=""
+  -DCMAKE_TOOLCHAIN_FILE="$PROJECT_TOOLCHAIN_FILE")
 
-  # iterate through $CMAKE_ARGS and convert to --cmake-opt format
-  for arg in "${cmake_args[@]}"; do
-    cmake_opt="--cmake-opt=$arg"
-    cmake_opts="$cmake_opts $cmake_opt"
-  done
+# iterate through $CMAKE_ARGS and convert to --cmake-opt format
+for arg in "${cmake_args[@]}"; do
+  cmake_opt="--cmake-opt=$arg"
+  cmake_opts="$cmake_opts $cmake_opt"
+done
 
-  configure_cmd="python repo/buildbot/configure.py 
+configure_cmd="python repo/buildbot/configure.py 
     --enable-all-llvm-targets 
     --shared-libs 
     --llvm-external-projects=clang-tools-extra 
@@ -62,7 +58,6 @@ if [ ! -f "$DPCPP_HOME/repo/build/CMakeCache.txt" ]; then
     --native_cpu 
     --cmake-gen=Ninja -o $DPCPP_BIN_DIR $cmake_opts"
 
-  echo "Running: $configure_cmd"
+echo "Running: $configure_cmd"
 
-  $configure_cmd
-fi
+$configure_cmd
