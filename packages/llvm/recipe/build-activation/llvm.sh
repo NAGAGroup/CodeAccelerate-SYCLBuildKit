e6ef0eec1b23dba3951b0811a3a05878ddcaca99
Jack <jackhmyers97@gmail.com>
2025-02-05 21:36:37 -0800

    wip: llvm builds using pixi build
    Signed-off-by: Jack <jackhmyers97@gmail.com>

--- packages/llvm/recipe/build-activation/llvm.sh
+++ packages/llvm/recipe/build-activation/llvm.sh
@@ -1,12 +1,13 @@
 set -e
 
-export DPCPP_HOME=${RECIPE_DIR}/..
-export DPCPP_BUILD="${DPCPP_HOME}/build"
 if [ "${LINUX_BUILD_ENV_ACTIVE:-0}" != "1" ]; then
   source "${DPCPP_HOME}/recipe/build-activation/linux.sh"
 fi
 
 if [ "${DPCPP_BUILD_ENV_ACTIVE:-0}" != "1" ]; then
+  export DPCPP_HOME=${RECIPE_DIR}/..
+  export DPCPP_BUILD="${DPCPP_HOME}/build"
+
   gcc_version=$(gcc -dumpversion)
   gcc_install_dir="${PREFIX}/lib/gcc/${CONDA_TOOLCHAIN_HOST}/${gcc_version}"
   export GCC_INSTALL_DIR="${gcc_install_dir}"
